82c1f53915fa5f02d512ad29b6451918b7537aa1
AryaBagusPutraPratama <bagusaria14@gmail.com>
2024-02-25 21:25:26 +0700

    Menampilkan view dari controller

--- routes/web.php
+++ routes/web.php
@@ -94,6 +94,4 @@ Route::resource('photos', PhotoController::class);
 Route::resource('photos', PhotoController::class)->only(['index',  'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
 
-Route::get('/greeting', function() {
-    return view('hello', ['name' => 'Arya']);
-});
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
